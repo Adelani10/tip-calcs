4a796471bcd25d61b99f1b943cf1bdf58f795ad3
Adelani <harounadelani@gmail.com>
2022-08-08 15:02:10 +0100

    nou

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let numberInput = document.querySelector(".number-input")
 let customInput = document.querySelector(".custom-input")
 let totalAmount = document.querySelector(".total-amount")
 let tipAmount = document.querySelector(".tip-amount")
-const btns = document.querySelectorAll(".btn")
+let btns = document.querySelectorAll(".btn")
 let resetBtn = document.querySelector(".reset-btn")
 const mainForm = document.querySelector(".main-form")
 
@@ -18,11 +18,16 @@ customInput.addEventListener("input", function(){
 })
 
 btns.forEach(function (btn, index){
-        btn.addEventListener("click", function(){
-        
+    btn.addEventListener("click", function(){
+        btns.forEach((item)=>{
+            if(item !== btn){
+                item.classList.remove("show")
+            }
+            console.log(item)
+        })
+        console.log(btn)
         if (index <= 2){
             counter = 5 * index + 5
-        
         }
         else if (index === 3){
             counter = 5 * index + 10
@@ -30,18 +35,19 @@ btns.forEach(function (btn, index){
         else{
             counter = 10 * index + 10
         }
+
+        btn.classList.add("show")
         
         })
-    })
+})
 numberInput.addEventListener ("input", function(){
-    let numberValue = parseFloat(numberInput.value)
-        let billValue = parseFloat(billInput.value)
-        currValue = (counter * billValue * numberValue)/100
-    tipAmount.innerHTML = `<h1>$${currValue}</h1>`
+        let numberValue = parseFloat(numberInput.value)
+    let billValue = parseFloat(billInput.value)
+    currValue = (counter * billValue * numberValue)/100
+    tipAmount .innerHTML = `<h1>$${currValue}</h1>`
     let grandTotal = currValue + billValue
     totalAmount.innerHTML = `<h1>$${grandTotal}</h1>`
 })
-
 
 resetBtn.addEventListener("click", function(){
     billInput.value = ''
